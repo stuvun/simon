483809ac7cfc163d2efc2e29855154b5169e06f9
Steven Kim <sskim14@gmail.com>
2019-10-22 10:22:40 -0400

    Planned game development

---
+++ script.js
@@ -0,0 +1,8 @@
+// SIMON GAME
+// 1. Press start
+// 2. Game starts
+// 3. Game displays sequence
+// 4. Player mimics sequence
+// 5. If player succeeds, the game displays the next sequence with one more step
+// 6. If player fails, game over
+// 7. Game continues until player fails.
