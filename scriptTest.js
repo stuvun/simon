00dd09058cfbdf35f201e204c8b1d92b53c3587e
Steven Kim <sskim14@gmail.com>
2019-10-23 14:12:17 -0400

    Fixed eventListener issue in scriptTest2.js

--- scriptTest.js
+++ scriptTest.js
@@ -9,10 +9,18 @@ var max = 4;
 
 const start = document.querySelector(".start");
 
+const retry = document.querySelector(".retry");
+
 start.addEventListener("click", function(evt) {
     evt.preventDefault();
     startGame();
     console.log("Game Starts!"); })
+
+retry.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    startNewGame();
+    console.log("New Game Starts!")
+})
 
 const gameOver = document.querySelector(".gameOver");
 
@@ -23,53 +31,81 @@ function flashButtons() {
         if (order.length < round) {
             order.push(JSON.stringify(Math.floor(Math.random() * (max - min + 1)) + min));
             console.log(order);
-            
-            order.forEach(function(seq) {
-                console.log(order[seq])
-            })
         } else { return }
     }, 200);
+
+    buttons.forEach(function(but) {
+        but.addEventListener("click", function() {
+            console.log(but.value);
+            console.log(round);
+
+            playerOrder.push(but.value);
+
+            if (playerOrder.length < order.length) {
+                console.log(playerOrder);
+                console.log(round);
+                checkOrder();
+                if (playerOrder.length == order.length - 1) {
+                console.log(playerOrder);
+                checkOrder();
+                flashButtons();
+                }
+            } else { return; }
+        })
+    })
 }
 
 function checkOrder() {
     if (JSON.stringify(order) == JSON.stringify(playerOrder)) {
         round = round + 1;
         console.log(round);
-    } else {
-        gameOver.style.visibility = "visible"}
+    } else if (JSON.stringify(order) != JSON.stringify(playerOrder)) {
+        gameOver.style.visibility = "visible";
         console.log(gameOver.style.visibility);
+        order.length = 0;
+        playerOrder.length = 0;
+        round = 1;
+        return {order, playerOrder, round};
+    } else { return }
 }
 
-function endGame() {
+// function endGame() {
 
-}
+// }
+
+// function startNewGame() {
+//     gameOver.style.visibility = "hidden";
+    
+//     round = 1;
+
+//     order.length = round - 1;
+
+//     playerOrder.length = 0;
+
+//     flashButtons();
+
+//     buttons.forEach(function(but) {
+//         but.addEventListener("click", function(evt) {
+//             evt.preventDefault();
+    
+//             console.log(but.value);
+    
+//             if (playerOrder.length < order.length - 1) {
+//                 playerOrder.push(but.value);
+//                 console.log(playerOrder);
+//                 console.log(round);
+//             } else if (playerOrder.length == order.length - 1) {
+//                 playerOrder.push(but.value);
+//                 console.log(playerOrder);
+//                 checkOrder();
+//                 playerOrder.length = 0;
+//             } else { return; }
+//         })
+//     })
+// }
 
 function startGame() {
-    gameOver.style.visibility = "hidden";
-
-    playerOrder.length = 0;
-
     flashButtons();
 
-    buttons.forEach(function(but) {
-        but.addEventListener("click", function(evt) {
-            evt.preventDefault();
-    
-            console.log(but.value);
-    
-            if (playerOrder.length < order.length - 1) {
-                playerOrder.push(but.value);
-                console.log(playerOrder);
-                console.log(round);
-            } else if (playerOrder.length == order.length - 1) {
-                playerOrder.push(but.value);
-                console.log(playerOrder);
-                checkOrder();
-                playerOrder.length = 0;
-                return;
-            } else { return; }
-        })
-    })
-
-
+    console.log(order)
 }
---
+++ scriptTest2.js
@@ -0,0 +1,81 @@
+let round = 1;
+let flash = round;
+let order = [];
+let playerOrder = [];
+let turn = 1;
+let restart = 0;
+const min = 1;
+const max = 4;
+
+const start = document.querySelector(".start");
+
+start.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    startGame();
+    console.log("Game Starts!"); })
+
+const gameOver = document.querySelector(".gameOver");
+
+const buttons = document.querySelectorAll(".button");
+
+function flashButtons() {
+    setInterval( function() {
+        if (order.length < round && turn != 0) {
+            order.push(JSON.stringify(Math.floor(Math.random() * (max - min + 1)) + min));
+            console.log(order);
+
+            order.forEach(function(seq) {
+                console.log(order[seq])
+            })
+
+            if (turn != 0) { turn = 0; }
+        } else { return }
+    }, 200);
+}
+
+function endGame() {
+    console.log("Click Start Game to retry!");
+}
+
+function checkOrder() {
+    if (JSON.stringify(order) != JSON.stringify(playerOrder)) {
+        gameOver.style.visibility = "visible";
+        console.log(gameOver.style.visibility);
+        endGame();
+        return;
+    } else {
+        round = round + 1;
+    }
+}
+
+function startGame() {
+    if (gameOver.style.visibility == "visible") {
+        order.length = 0; round = 1 }
+
+    gameOver.style.visibility = "hidden";
+
+    playerOrder.length = 0;
+
+    flashButtons();
+}
+
+buttons.forEach(function(but) {
+    but.addEventListener("click", function(evt) {
+        evt.preventDefault();
+
+        console.log(but.value);
+
+        if (playerOrder.length < order.length - 1) {
+            playerOrder.push(but.value);
+            console.log(playerOrder);
+            console.log(round);
+        } else if (playerOrder.length == order.length - 1) {
+            playerOrder.push(but.value);
+            console.log(playerOrder);
+            checkOrder();
+            playerOrder.length = 0;
+            turn = turn = 1;
+            return;
+        } else { return; }
+    })
+})
